ada25d101bc8986405c8e79acdffcbc515196ee7
Kacper <kusiakkacper@gmail.com>
2021-06-20 20:33:39 +0200

    Remove more unused imports

--- lib/services/authApiService.dart
+++ lib/services/authApiService.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
 import 'dart:developer';
 
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
